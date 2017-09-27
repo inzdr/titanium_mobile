970b54467b942710cf4b76b6f5acbcfaac548ae1
Richard Lustemberg <richard@lustembergconsultancy.com>
2017-09-27 18:45:25 +0200

    fixed wrong usage of NSInteger

--- iphone/Classes/TiApp.m
+++ iphone/Classes/TiApp.m
@@ -831,12 +831,13 @@ TI_INLINE void waitForMemoryPanicCleared();   //WARNING: This must never be run 
         [dict addEntriesFromDictionary:errorinfo];
     } else {
         NSMutableDictionary *responseObj = [uploadTaskResponses objectForKey:@(task.taskIdentifier)];
-        NSString *responseText = nil;
-        NSInteger  *statusCode = nil;
+        
         if (responseObj) {
+            NSString *responseText = nil;
+            NSInteger  statusCode = 0;
             //we only send responseText as this is the responsesData dictionary only gets filled with data from uploads
             responseText = [[NSString alloc] initWithData:[responseObj objectForKey:@"responseData"] encoding:NSUTF8StringEncoding];
-            statusCode = (NSInteger*)[responseObj objectForKey:@"statusCode"];
+            statusCode = (NSInteger)[responseObj objectForKey:@"statusCode"];
             
             [uploadTaskResponses removeObjectForKey:@(task.taskIdentifier)];
         }
@@ -844,7 +845,7 @@ TI_INLINE void waitForMemoryPanicCleared();   //WARNING: This must never be run 
         NSDictionary * success = [NSMutableDictionary dictionaryWithObjectsAndKeys:NUMBOOL(YES), @"success",
                                    NUMINT(0), @"errorCode",
                                    responseText,@"responseText",
-                                   statusCode,@"statusCode",
+                                   NUMINTEGER(statusCode),@"statusCode",
                                    nil];
         [dict addEntriesFromDictionary:success];
     }
