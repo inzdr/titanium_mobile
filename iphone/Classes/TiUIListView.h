ce4da909519f2e90d005c635b150c62e3a6d0f9c
Richard Lustemberg <richard@lustembergconsultancy.com>
2019-01-24 23:37:40 +0100

    fix(ios): Added handling of bindId to previewContext
    # Conflicts:
    #	iphone/Classes/TiUIListView.m

--- iphone/Classes/TiUIListView.h
+++ iphone/Classes/TiUIListView.h
@@ -1,13 +1,17 @@
 /**
  * Appcelerator Titanium Mobile
- * Copyright (c) 2013 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2009-2019 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
+ *
+ * WARNING: This is generated code. Modify at your own risk and without support.
  */
 #ifdef USE_TI_UILISTVIEW
 
 #import "TiUIListViewProxy.h"
 #import <TitaniumKit/TiUIView.h>
+
+TiViewProxy *FindViewProxyWithBindIdContainingPoint(UIView *view, CGPoint point);
 
 @interface TiUIListView : TiUIView <UITableViewDelegate, UITableViewDataSource, UITableViewDataSourcePrefetching, UIScrollViewDelegate, UIGestureRecognizerDelegate, UISearchBarDelegate, UISearchResultsUpdating, UISearchControllerDelegate, TiScrolling, TiProxyObserver, TiUIListViewDelegateView>
 
