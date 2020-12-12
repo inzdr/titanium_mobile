86b05dd8f7a692573cb5746908b6b64a1dc462d6
Rodrigo Farfán Flores <phobeous@gmail.com>
2020-12-12 13:19:28 +0100

    - Remaining custom changes to SDK

--- android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
+++ android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
@@ -1607,6 +1607,20 @@ public abstract class TiUIView implements KrollProxyListener, OnFocusChangeListe
 		} else {
 			data.put(TiC.EVENT_PROPERTY_SIZE, (double) 0);
 		}
+
+		// phobeous
+		if (dictToCopy.containsKey("globalX")) {
+			data.put("globalX", dictToCopy.get("globalX"));
+		} else {
+			data.put("globalX", (double) 0);
+		}
+		if (dictToCopy.containsKey("globalY")) {
+			data.put("globalY", dictToCopy.get("globalY"));
+		} else {
+			data.put("globalY", (double) 0);
+		}
+		// end phobeous
+
 		data.put(TiC.EVENT_PROPERTY_SOURCE, proxy);
 		return data;
 	}
@@ -1757,7 +1771,13 @@ public abstract class TiUIView implements KrollProxyListener, OnFocusChangeListe
 
 			public boolean onTouch(View view, MotionEvent event)
 			{
-				if (event.getAction() == MotionEvent.ACTION_UP) {
+				// 2019.08.27 - phobeous@iNZDR : updated this block from 5.X to 8.1.X
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					Log.d(TAG, "phobeous@iNZDR -> eventX: " + ((double) event.getRawX())
+								   + ", eventY: " + ((double) event.getRawY()));
+					lastUpEvent.put("globalX", (double) event.getRawX());
+					lastUpEvent.put("globalY", (double) event.getRawY());
+				} else if (event.getAction() == MotionEvent.ACTION_UP) {
 					TiDimension xDimension = new TiDimension((double) event.getX(), TiDimension.TYPE_LEFT);
 					TiDimension yDimension = new TiDimension((double) event.getY(), TiDimension.TYPE_TOP);
 					lastUpEvent.put(TiC.EVENT_PROPERTY_X, xDimension.getAsDefault(view));
